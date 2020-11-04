d04fad021242120001301a5f61fa0e46cd542d32
ChinHaoX3 <40873148+ChinHaoX3@users.noreply.github.com>
2020-11-04 20:35:47 +0800

    Update Index.java

--- src/entity/Index.java
+++ src/entity/Index.java
@@ -5,11 +5,9 @@ import java.time.LocalDateTime;
 public class Index {
 	private int vacancies;
 	private String getName;
-	private LocalDateTime tutorialSlot_start;
-	private LocalDateTime tutorialSlot_end;
-	private LocalDateTime labSlot_start;
-	private LocalDateTime labSlot_end;
-	private LocalDateTime lectureSlot_end;
+	private String tutorialSlot;
+	private String labSlot;
+	private String lectureSlot;
 
 	public int getVacancies() {
 		return vacancies;
@@ -27,44 +25,28 @@ public class Index {
 		this.getName = getName;
 	}
 
-	public LocalDateTime getTutorialSlot_start() {
-		return tutorialSlot_start;
+	public String getTutorialSlot() {
+		return tutorialSlot;
 	}
 
-	public void setTutorialSlot_start(LocalDateTime tutorialSlot_start) {
-		this.tutorialSlot_start = tutorialSlot_start;
+	public void setTutorialSlot(String tutorialSlot) {
+		this.tutorialSlot = tutorialSlot;
 	}
 
-	public LocalDateTime getTutorialSlot_end() {
-		return tutorialSlot_end;
+	public String getLabSlot() {
+		return labSlot;
 	}
 
-	public void setTutorialSlot_end(LocalDateTime tutorialSlot_end) {
-		this.tutorialSlot_end = tutorialSlot_end;
+	public void setLabSlot(String labSlot) {
+		this.labSlot = labSlot;
 	}
 
-	public LocalDateTime getLabSlot_start() {
-		return labSlot_start;
+	public String getLectureSlot() {
+		return lectureSlot;
 	}
 
-	public void setLabSlot_start(LocalDateTime labSlot_start) {
-		this.labSlot_start = labSlot_start;
-	}
-
-	public LocalDateTime getLabSlot_end() {
-		return labSlot_end;
-	}
-
-	public void setLabSlot_end(LocalDateTime labSlot_end) {
-		this.labSlot_end = labSlot_end;
-	}
-
-	public LocalDateTime getLectureSlot_end() {
-		return lectureSlot_end;
-	}
-
-	public void setLectureSlot_end(LocalDateTime lectureSlot_end) {
-		this.lectureSlot_end = lectureSlot_end;
+	public void setLectureSlot(String lectureSlot) {
+		this.lectureSlot = lectureSlot;
 	}
 
 	public boolean checkVacancy() {
