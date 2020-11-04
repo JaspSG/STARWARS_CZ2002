39f60edd1735129f35151d1c7d5e3be1ddd1656b
JaspSG <54952087+JaspSG@users.noreply.github.com>
2020-11-04 23:50:28 +0800

    added Course reader

--- src/control/fileManager.java
+++ src/control/fileManager.java
@@ -7,18 +7,24 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
+import entity.Course;
 import entity.Student;
 
 public class fileManager {
 	
+
+	
+	String outStudentFileName = "StudentFile";
+	String outCoursesFileName = "CoursesFile";
+	
 	//STUDENT OBJECTS READER
 	
-	public void saveFile(String filename, ArrayList<Student> listOfStudent) throws Exception {
+	public void saveStudentFile(ArrayList<Student> listOfStudent) throws Exception {
 		
-		String outputfilepath = String.format("src\\\\%s.ser", filename);
+		String outputfilepath = String.format("src\\\\%s.ser", this.outStudentFileName);
 		
 	    FileOutputStream fileOut =
-	    new FileOutputStream("outputfilepath");
+	    new FileOutputStream(outputfilepath);
 	    ObjectOutputStream out = new ObjectOutputStream(fileOut);
 	    out.writeObject(listOfStudent);
 	    out.close();
@@ -26,9 +32,9 @@ public class fileManager {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public ArrayList<Student> loadFile(String filename) {
+	public ArrayList<Student> loadStudentFile() {
 		
-		String filepath = String.format("src\\\\%s.ser", filename);
+		String filepath = String.format("src\\\\%s.ser", this.outStudentFileName);
 		ArrayList<Student> value = new ArrayList<Student>();
 		
 	      try {
@@ -48,6 +54,45 @@ public class fileManager {
 	}
 	
 	// END OF STUDENT OBJECT READER
+	
+	// COURSE OBJECT READER
+	
+	public void saveCoursesFile(ArrayList<Course> listOfCourses) throws Exception {
+		
+		String outputfilepath = String.format("src\\\\%s.ser", this.outCoursesFileName);
+		
+	    FileOutputStream fileOut =
+	    new FileOutputStream(outputfilepath);
+	    ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	    out.writeObject(listOfCourses);
+	    out.close();
+	    fileOut.close();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<Course> loadCoursesFile() {
+		
+		String filepath = String.format("src\\\\%s.ser", this.outCoursesFileName);
+		ArrayList<Course> value = new ArrayList<Course>();
+		
+	      try {
+	          FileInputStream fileIn = new FileInputStream(filepath);
+	          ObjectInputStream in = new ObjectInputStream(fileIn);
+	          value = (ArrayList<Course>) in.readObject();
+	          in.close();
+	          fileIn.close();
+	       } catch (IOException i) {
+	          i.printStackTrace();
+	       } catch (ClassNotFoundException c) {
+	          System.out.println("Course class not found");
+	          c.printStackTrace();
+	       }
+	      
+	      return value;
+	}
+	
+	// END OF COURSE OBJECT READER
+	
 	
 
 
