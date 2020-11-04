95d9420835946bbcb37eaffd444e112b7f6c8111
JaspSG <54952087+JaspSG@users.noreply.github.com>
2020-11-04 19:42:49 +0800

    control folder
    Add control folder

---
+++ src/control/fileManager.java
@@ -0,0 +1,13 @@
+package control;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class fileManager {
+
+	
+	
+}
