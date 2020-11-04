87fee910821857821d108dc8dca00429ff3c8b2a
Raymond G <53110445+duckygoh@users.noreply.github.com>
2020-11-04 17:43:01 +0800

    Create Index.java
    你好

---
+++ src/Index.java
@@ -0,0 +1,12 @@
+
+public class Index {
+	private int vacancies;
+	private String getName;
+	private Tutorial tutorialSlot;
+	private Lab labSlot;
+	private Lecture lectureSlot;
+	
+	public boolean checkVacancy() {
+		//return
+	}
+}
