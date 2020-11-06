2b857e31d8111067c4bf2cb0a2a782ad13610ad0
gazelle124 <quah124@hotmail.sg>
2020-11-06 18:44:16 +0800

    sample for global index

--- src/entity/User.java
+++ src/entity/User.java
@@ -26,9 +26,11 @@ abstract class User {
 	String loginID;
 	String loginPW;
 	
-	public static void validateLogin(String password) throws NoSuchAlgorithmException
+	public static  int validateLogin(String password) throws NoSuchAlgorithmException
 	{
 		//Boolean isValid = false;
+			int studentIndex = -1;
+		  
 		
 		  MessageDigest md = MessageDigest.getInstance("SHA-256");
 		    //String text = "Text to hash, cryptographically.";
@@ -39,6 +41,23 @@ abstract class User {
 
 		    String hex = String.format("%064x", new BigInteger(1, digest));
 		    System.out.println(hex);
+		     
+		    for(int i =0; i < studentList.size;i++)
+		    {
+		    	String currentPassword = studentList.get(i).getPassword();
+		    	 md.update(currentPassword.getBytes(StandardCharsets.UTF_8));
+				 byte[] currentDigest = md.digest();
+
+				 String currentHex = String.format("%064x", new BigInteger(1, currentDigest));
+				 
+				 if(hex.equals(currentHex))
+				 {
+					 return i;
+				 }
+		    	
+		    }
+		    
+		    return studentIndex;
 		
 		//return isValid;
 	
