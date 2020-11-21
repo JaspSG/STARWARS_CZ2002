76ad4c3532f43f07043dd359759b5c43f3357561
ChinHaoX3 <40873148+ChinHaoX3@users.noreply.github.com>
2020-11-21 17:22:06 +0800

    update

--- src/boundary/AdminUI.java
+++ src/boundary/AdminUI.java
@@ -508,7 +508,9 @@ public class AdminUI {
 			System.out.println("Returning to main UI....\n");
 			return;
 		}
-		CourseManager.printCourseStudentList(courseID);
+		else {
+			CourseManager.printCourseStudentList(courseID);
+		}
 	}
 
 	public static void updateAccessPeriod(Scanner sc) throws ParseException {
