d232f8ed011db4c7865b72d73a238843a0b77aec
JaspSG <54952087+JaspSG@users.noreply.github.com>
2020-11-24 11:40:41 +0800

    Revert "added javadoc"
    This reverts commit 3c5180f018959e2e0c30020b4bcfe1baf87ed4a7.

--- src/control/fileManager.java
+++ src/control/fileManager.java
@@ -15,11 +15,6 @@ public class fileManager {
 
 	// STUDENT OBJECTS READER
 
-	/***
-	 * This method saves the list of all the students' information into a file
-	 * @param listOfStudent This contains the list of students
-	 * @throws Exception To catch errors when file does not save properly
-	 */
 	public static void saveStudentFile(ArrayList<Student> listOfStudent) throws Exception {
 
 		String outputfilepath = String.format("src\\\\%s.ser", OUTSTUDENTFILENAME);
@@ -30,10 +25,7 @@ public class fileManager {
 		out.close();
 		fileOut.close();
 	}
-	/***
-	 * This method loads the information of all the students from a file
-	 * @return
-	 */
+
 	@SuppressWarnings("unchecked")
 	public static ArrayList<Student> loadStudentFile() {
 
@@ -59,12 +51,6 @@ public class fileManager {
 	// END OF STUDENT OBJECT READER
 
 	// COURSE OBJECT READER
-	
-	/***
-	 * This method saves the list of all the courses information into a file
-	 * @param listOfCourses The list of all courses information
-	 * @throws Exception To catch errors when file does not save properly
-	 */
 
 	public static void saveCoursesFile(ArrayList<Course> listOfCourses) throws Exception {
 
@@ -76,11 +62,6 @@ public class fileManager {
 		out.close();
 		fileOut.close();
 	}
-	
-	/***
-	 * This method loads the list of all course information from a file
-	 * @return
-	 */
 
 	@SuppressWarnings("unchecked")
 	public static ArrayList<Course> loadCoursesFile() {
@@ -107,12 +88,7 @@ public class fileManager {
 	// END OF COURSE OBJECT READER
 
 	// ADMIN OBJECT READER
-	
-	/***
-	 * This method saves the list of all the admin information into a file
-	 * @param listOfAdmin The list of all admins information
-	 * @throws Exception To catch errors when file does not save properly
-	 */
+
 	public static void saveAdminFile(ArrayList<Admin> listOfAdmin) throws Exception {
 
 		String outputfilepath = String.format("src\\\\%s.ser", OUTADMINFILENAME);
@@ -123,11 +99,7 @@ public class fileManager {
 		out.close();
 		fileOut.close();
 	}
-	
-	/***
-	 * This method loads the list of all the admin information from a file
-	 * @return
-	 */
+
 	@SuppressWarnings("unchecked")
 	public static ArrayList<Admin> loadAdminFile() {
 
